70f9067bbc55abdd8007b7215b79701bd4e37d4c
vslongo <vslongo@gmail.com>
2023-11-15 16:33:24 -0300

    relating models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,11 @@
-// src/models/index.ts
-
 import { Category } from './Category'
+import { Course } from './Course'
+
+Category.hasMany(Course)
+
+Course.belongsTo(Category)
 
 export {
+  Course,
   Category
 }
