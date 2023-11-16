f1a01c228e889f1a1ed092d82fd6043ff851fef7
vslongo <vslongo@gmail.com>
2023-11-15 21:09:35 -0300

    get information about course

--- src/controllers/categories-controller.ts
+++ src/controllers/categories-controller.ts
@@ -1,10 +1,8 @@
-// src/controllers/categories-controller.ts
-
 import { Request, Response } from 'express'
 import { getPaginationParams } from '../helpers/getPaginationParams'
 import { categoryService } from '../services/categoryService'
 
-const categoriesController = {
+export const categoriesController = {
   index: async (req: Request, res: Response) => {
     const [page, perPage] = getPaginationParams(req.query)
 
@@ -17,7 +15,18 @@ const categoriesController = {
         return res.status(400).json({ message: err.message })
       }
     }
+  },
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const category = await categoryService.findByIdWithCourses(id)
+      return res.json(category)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
   }
 }
 
-export { categoriesController }
