7a90ae10dafc22fd87e36f369d4169a6205e9ce5
vslongo <vslongo@gmail.com>
2023-11-15 22:14:04 -0300

    user register

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import { authController } from './controllers/authController'
 import { categoriesController } from './controllers/categories-controller'
 import { coursesController } from './controllers/courseController'
+import { episodesController } from './controllers/episodesController'
 
 const router = express.Router()
+
+router.post('/auth/register', authController.register)
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
@@ -12,5 +16,7 @@ router.get('/courses/featured', coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
+
+router.get('/episodes/stream', episodesController.stream)
 
 export { router }
