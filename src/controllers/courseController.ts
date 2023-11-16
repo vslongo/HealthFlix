4c85a9d8938802dc6903891cf9a2589c737f3d64
vslongo <vslongo@gmail.com>
2023-11-15 21:37:14 -0300

    search with name course

--- src/controllers/courseController.ts
+++ src/controllers/courseController.ts
@@ -2,6 +2,7 @@
 
 import { Request, Response } from 'express'
 import { courseService } from '../services/courseService'
+import { getPaginationParams } from '../helpers/getPaginationParams'
 
 export const coursesController = {
   // GET /courses/:id
@@ -35,6 +36,22 @@ export const coursesController = {
       return res.json(newestCourses)
     } catch (err) {
 			if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // GET /courses/search
+  search: async (req: Request, res: Response) => {
+    const { name } = req.query
+    const [page, perPage] = getPaginationParams(req.query)
+
+    try {
+			if (typeof name !== 'string') throw new Error('name param must be of type string');
+      const courses = await courseService.findByName(name, page, perPage)
+      return res.json(courses)
+    } catch (err) {
+      if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
       }
     }
