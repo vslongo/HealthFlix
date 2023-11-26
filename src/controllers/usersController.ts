fe4d09475bac3cd221130ac792eef0691c8fca3c
vslongo <vslongo@gmail.com>
2023-11-26 19:31:08 -0300

    updating user information

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -15,6 +15,28 @@ export const usersController = {
       }
     }
   },
+
+  update: async (req: AuthenticateRequest, res: Response) => {
+    const { id } = req.user!
+    const { firstName, lastName, phone, email, birth } = req.body
+
+    try {
+      const updatedUser = await userService.update(id, {
+        firstName,
+        lastName,
+        phone,
+        email,
+        birth
+      })
+
+      return res.json(updatedUser)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
   // GET /users/current/watching
   watching: async (req: AuthenticateRequest, res: Response) => {
     const { id } = req.user!
--- src/routes.ts
+++ src/routes.ts
@@ -36,5 +36,6 @@ router.delete('/likes', ensureAuth, likesController.delete)
 
 router.get('/users/current', ensureAuth, usersController.show)
 router.get('/users/current/watching', ensureAuth, usersController.watching)
+router.put('/users/current', ensureAuth, usersController.update)
 
 export { router }
